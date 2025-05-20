5a944bb9bd939c3325fd1515aece83a7ab6376d1
Hezekiel112 <127784200+hezekiel112@users.noreply.github.com>
2025-05-20 15:20:26 +0200

    Ajout CloudCode - Authentification Joueur

---
+++ Assets/Script/Networking/Authentification/KSAuthentificationHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KemiaSimulatorCore.Script.Networking.Authentication
+{
+    /// <summary>
+    ///     Ce script gère les fonctionnalités sign-in et sign-up
+    /// </summary>
+    public class KSAuthentificationHandler : MonoBehaviour
+    {
+        
+    }
+}
