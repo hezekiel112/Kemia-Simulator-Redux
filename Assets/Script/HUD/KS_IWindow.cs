8175092b5922b963345dd64a8eeb4b7c03b22aa6
Hezekiel112 <127784200+hezekiel112@users.noreply.github.com>
2025-05-20 13:52:02 +0200

    Removed old scripting backend

--- Assets/Script/HUD/KS_IWindow.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface KS_IWindow
-{
-    public virtual void Show() { }
-    public virtual void Hide() { }
-    public virtual void InitializeWindow() {  }
-}
--- Assets/Script/Utils/DevMod/IDevModHandler.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// Interface de fonctions DevMod
-/// </summary>
-public interface IDevModHandler
-{
-    public virtual void OnStart(bool active) { }
-    public virtual void OnUpdate(bool active) { }
-}
