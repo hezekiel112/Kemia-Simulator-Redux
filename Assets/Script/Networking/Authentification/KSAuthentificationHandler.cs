0b4024cd9ada2a890fe15982c06fa8eb56ff5b60
Hezekiel112 <127784200+hezekiel112@users.noreply.github.com>
2025-05-20 15:31:57 +0200

    Cleaned Project (Again)

--- Assets/Script/Networking/Authentification/KSAuthentificationHandler.cs
+++ Assets/Script/Networking/Authentification/KSAuthentificationHandler.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using KemiaSimulatorCore.Script.Statics;
 using UnityEngine;
 
 namespace KemiaSimulatorCore.Script.Networking.Authentication
@@ -9,6 +11,8 @@ namespace KemiaSimulatorCore.Script.Networking.Authentication
     /// </summary>
     public class KSAuthentificationHandler : MonoBehaviour
     {
-        
+        private void Awake(){
+            this.kslog(SystemInfo.deviceUniqueIdentifier);
+        }
     }
 }
